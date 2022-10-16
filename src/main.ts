094bfd94767b7357891c37a288d9ca6917eebd70
Patryk Bujanowski <bujanowski.patryk@outlook.com>
2022-10-16 11:40:24 +0200

    Added CORS.

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,11 @@ import { AppModule } from './app.module';
 import { LoggerService } from './logger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { bufferLogs: true });
-  app.useLogger(new LoggerService());
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+    cors: true,
+    logger: new LoggerService(),
+  });
   await app.listen(5000);
 }
 bootstrap();
